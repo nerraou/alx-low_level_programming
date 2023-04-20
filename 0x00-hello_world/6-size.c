011a9cf5442c2b8a036e623b0b74686529f1f302
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-20 04:38:07 -0700

    add float type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,9 @@
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
-	printf("Size of a int: %zu byte(s)\n", sizeof(int));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
