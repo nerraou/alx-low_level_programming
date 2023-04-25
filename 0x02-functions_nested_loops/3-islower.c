97a6a473397ea4a45ff01d83ddf662b90c96ce62
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-25 14:56:23 -0700

    is lower function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - check iis lower
+ * @c: arg
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+
+		return (1);
+	}
+	return (0);
+}
+
