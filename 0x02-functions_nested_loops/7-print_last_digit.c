d23269d875a0c6430a35bd8faef062f34eb3b60c
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-25 15:31:33 -0700

    get abs before extracting last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,7 +9,9 @@ int print_last_digit(int n)
 {
 	int	last_digit;
 
-	last_digit = _abs(n % 10);
+	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit = -last_digit;
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
