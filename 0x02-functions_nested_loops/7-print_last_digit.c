63f1a0e787dd17847ea00b097b0fe47b1b9d3142
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-25 15:27:11 -0700

    print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: integer
+ * Return: absolute value
+ */
+int print_last_digit(int n)
+{
+	int	last_digit;
+
+	last_digit = n % 10;
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
