fcd67cf9ab1aaf1ef9316e449eef65a9df7b4dfe
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-25 15:54:16 -0700

    handle zero printing

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,11 +3,15 @@
 /**
  * print_two_digits - print two digit number
  * @n: two digit number
+ * @print_pad: boolean print pad or not
  */
-void print_two_digits(int n)
+void print_two_digits(int n, int print_pad)
 {
 	if (n <= 9)
-		_putchar(' ');
+	{
+		if (print_pad)
+			_putchar(' ');
+	}
 	else
 		_putchar((n / 10) + '0');
 	_putchar((n % 10) + '0');
@@ -18,8 +22,8 @@ void print_two_digits(int n)
  */
 void times_table(void)
 {
-	int	i;
-	int	j;
+	int i;
+	int j;
 
 	for (i = 0; i <= 9; i++)
 	{
@@ -30,7 +34,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			print_two_digits(i * j);
+			print_two_digits(i * j, j != 0);
 		}
 		_putchar('\n');
 	}
