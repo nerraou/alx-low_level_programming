13f504530fa073800577fa6b45389a1c9d092172
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-25 16:26:12 -0700

    fix n greater then 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,11 +6,21 @@
  */
 void print_to_98(int n)
 {
-	for (; n <= 98; n++)
+	int step;
+
+	if (n < 98)
+		step = 1;
+	else
+		step = -1;
+	while (1)
 	{
 		printf("%d", n);
 		if (n != 98)
 			printf(", ");
+
+		if (n == 98)
+			break;
+		n += step;
 	}
 	printf("\n");
 }
