902b10788336385809a15cc65a0442dedc5568c9
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-25 14:48:10 -0700

    print 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,3 +13,16 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 }
+
+/**
+ * print_alphabet_x10 - print alphabet 10
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
