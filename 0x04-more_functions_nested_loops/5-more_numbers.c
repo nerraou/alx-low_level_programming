a8dbe78421dec3e5fb94a32534cbf77440653dc6
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-27 09:08:51 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,17 @@
 void more_numbers(void)
 {
 	int num;
+	int i;
 
-	for (num = 0; num <= 14; num++)
+	for (i = 0; i < 10; i++)
 	{
-		if (num >= 10)
-			_putchar(num / 10 + 48);
-		_putchar(num % 10 + 48);
+		for (num = 0; num <= 14; num++)
+		{
+			if (num >= 10)
+				_putchar(num / 10 + 48);
+			_putchar(num % 10 + 48);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 	return (0);
 }
