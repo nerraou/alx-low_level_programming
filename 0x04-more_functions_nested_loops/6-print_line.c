a0bb2716f0b9117271f1f6058b393cc8cfff42a2
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-27 09:15:06 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print_line
+ * @n: arg
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('-');
+	}
+	putchar('\n');
+}
