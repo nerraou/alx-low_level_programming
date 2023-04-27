00870b9c600e402418cc7d82a2bb0f957f545c8d
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-27 09:02:50 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * more_numbers - print numbers
+ */
+
+void more_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 14; num++)
+	{
+		if (num >= 10)
+			_putchar(num / 10 + 48);
+		_putchar(num % 10 + 48);
+	}
+	_putchar('\n');
+	return (0);
+}
