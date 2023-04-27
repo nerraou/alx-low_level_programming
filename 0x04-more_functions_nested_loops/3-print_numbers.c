c86826cab5cdf9e11d0817e44cbe4b3e62963398
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-04-27 07:45:49 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ */
+
+void print_numbers(void)
+{
+	char num;
+
+	for (num = '0'; i <= '9'; i++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
