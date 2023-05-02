ec06dd675a1cd4a9c49e367d09028e9ab6781c20
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-02 06:24:18 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _atoi - get number
+ *@s: arg1
+ *Return: number
+ */
+int _atoi(char *s)
+{
+	int	i;
+	int	signe;
+	int	num;
+
+	i = 0;
+	signe = 1;
+	num = 0;
+	while ((s[i] >= 7 && s[i] <= 13) || s[i] == ' ')
+		i++;
+	if (s[i] == '+')
+		i++;
+	else if (s[i] == '-')
+	{
+		i++;
+		signe = -1;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		num = num * 10 + s[i] - '0';
+		i++;
+	}
+	return (num * signe);
+}
