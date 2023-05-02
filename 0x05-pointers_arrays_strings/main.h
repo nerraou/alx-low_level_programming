98f30d63d9939c1db27dc93472a7ab27e550dc61
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-02 02:43:07 -0700

    add putchar to main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char c);
 
 #endif /* MAIN_H */
