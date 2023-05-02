895dae6177313f07813178d5073f7ca803618b7e
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-02 02:52:44 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strlen - string length
+ *@s: arg
+ *Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * print_rev - print rev
+ *@s: arg
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+	while (i > 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
