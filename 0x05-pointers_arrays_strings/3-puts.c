145fcf72d6784295df0521f6895fc89d2e0b040f
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-02 02:38:25 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - print string
+ *@str: arg
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
