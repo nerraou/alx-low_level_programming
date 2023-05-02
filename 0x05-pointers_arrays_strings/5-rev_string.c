5fef3fe7b05eded1b11f4d353ca74d65a883eecd
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-02 03:25:56 -0700

    firx

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,24 @@
 #include "main.h"
+
+/**
+ * _strlen - string length
+ *@s: arg
+ *Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
 /**
  * rev_string - rev string
  *@s: arg
