da7e7f6a6e673d4ae00f37de20803c608e48153d
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-04 05:26:42 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+
+/**
+ * _strlen - string length
+ *@s: arg
+ *Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strcat - puts2
+ *@src: arg1
+ *@dest: arg1
+ *Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len;
+	int i;
+
+	i = 0;
+	len = 0;
+	len = lent(dest);
+	while (src[i])
+	{
+		dest[len] = src[i];
+		i++;
+		len++;
+	}
+	dest[len] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdio.h>
+
+int _strlen(char *s);
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
