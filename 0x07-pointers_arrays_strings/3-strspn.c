22de3c4f94af2b132b4986170ed52b2c87274ecc
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-08 03:51:26 -0700

    fix: strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,9 +15,11 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (accept[j] != s[i])
-				return (i);
+			if (accept[j] == s[i])
+				break;
 		}
+		if (accept[j] == '\0')
+			return (i);
 	}
 	return (i);
 }
