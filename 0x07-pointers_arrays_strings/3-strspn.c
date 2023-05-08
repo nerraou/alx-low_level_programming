be50b195e2f01995a8fa8f416930ff0cdba13d5b
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-08 03:27:40 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strspn - span a string
+ *@s: string
+ *@accept: charset
+ *Return: index of the first not exist character
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	unsigned int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] != s[i])
+				return (i);
+		}
+	}
+	return (i);
+}
