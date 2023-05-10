045d8b2a5538a7562b4bd79cc303aaaca8130116
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-10 03:04:15 -0700

    _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  _pow_recursion - power recursion
+ *@x: base
+ *@y: power
+ *Return: x^y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
