f3d773fdcbe16b177a0f5618bf3f494a9085798c
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-12 08:50:31 -0700

    print error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,6 +11,11 @@ int main(int argc, char const *argv[])
 	if (argc == 3)
 	{
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
 	}
-	return (0);
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
