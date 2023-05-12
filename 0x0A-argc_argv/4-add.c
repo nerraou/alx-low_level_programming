1d6dfb03984208215ff05afc4b3f7e675bbc44c2
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-12 09:10:43 -0700

    handles error

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,21 +1,46 @@
 #include "main.h"
 
 /**
- * main - add two args
+ * has_only_digits - check if str contains only digits
+ * @str: string
+ * Return: 1 or 0
+ */
+int has_only_digits(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - add args
  * @argc: arguments count
  * @argv: arguments value
- * Return: Always 0 (Success)
+ * Return: 1 or 0
  */
 int main(int argc, char const *argv[])
 {
-	if (argc == 3)
+	int sum;
+	int i;
+
+	sum = 0;
+	if (argc >= 3)
 	{
-		printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
-		return (0);
+		for (i = 1; i < argc; i++)
+		{
+			if (!has_only_digits(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
 	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
+	printf("%d\n", sum);
+	return (0);
 }
