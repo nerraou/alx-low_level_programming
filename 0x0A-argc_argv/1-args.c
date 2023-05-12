e35f94155c1a24dc87a8c270a16ab473126f50e2
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-12 08:15:07 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - prints program name
+ * @argc: arguments count
+ * @argv: arguments value
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
