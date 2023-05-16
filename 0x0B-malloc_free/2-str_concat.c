4261b5cad1f96dfc5477b2c4eeeac88be3af3237
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-16 02:28:40 -0700

    concat two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * _strlen - string length
+ *@s: arg
+ *Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * str_concat - concat two strings
+ * @s1: arg1
+ * @s2: arg2
+ *Return: concatinated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len_s1;
+	int len_s2;
+	int i;
+	char *dest;
+
+	len_s1 = _strlen(s1);
+	len_s2 = _strlen(s2);
+	dest = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	if (!dest)
+		return (NULL);
+	for (i = 0; i < len_s1; i++)
+	{
+		dest[i] = s1[i];
+	}
+	for (i = 0; i < len_s2; i++)
+	{
+		dest[i + len_s1] = s2[i];
+	}
+	dest[len_s1 + len_s2] = '\0';
+	return (dest);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -6,5 +6,6 @@
 
 char *create_array(unsigned int size, char c);
 int _strlen(char *s);
+char *str_concat(char *s1, char *s2);
 
 #endif /* MAIN_H */
