0b61ce158ff8a1431ee5fb114c4ba30e309dd270
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-22 02:46:15 -0700

    init dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * init_dog - init dog struct
+ * @d: dog struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->age = age;
+	d->name = name;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,8 @@
+ifndef MAIN_H
+#define MAIN_H
+
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* MAIN_H */
