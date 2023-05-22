a541de2938ef3969c91f5f77cf8018bd0a8ef6d1
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-22 03:24:28 -0700

    init struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,50 @@
 #include "main.h"
 
 /**
+ * _strlen - string length
+ *@s: arg
+ *Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * _strdup - copy an array
+ * @str: arg
+ *Return: allocated array
+ */
+char *_strdup(char *str)
+{
+	int len;
+	int i;
+	char *dest;
+
+	if (!str)
+		return (NULL);
+	len = _strlen(str);
+	dest = (char *)malloc(sizeof(char) * len + 1);
+	if (!dest)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = str[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * init_dog - init dog struct
  * @d: dog struct
  * @name: dog name
@@ -10,6 +54,6 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	d->age = age;
-	d->name = name;
+	d->name = _strdup(name);
 	d->owner = owner;
 }
