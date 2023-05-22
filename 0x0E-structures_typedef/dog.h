73aaf2786a4a01cac40cbd3e86e920ff3b1538bb
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-22 04:29:41 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,12 +13,12 @@
  * Description: dog description
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
