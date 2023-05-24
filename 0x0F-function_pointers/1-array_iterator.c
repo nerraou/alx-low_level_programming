f16d6a0eecc26e3eee58d545e70a1cdbd06f08a3
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-24 03:30:48 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - iter to an array
+ *@array: array
+ *@size: array size
+ *@action: function pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (size <= 0 || !action)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
