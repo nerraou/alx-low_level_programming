0af78caa9f41c13195c8cf892489889fb2649f57
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-24 03:32:42 -0700

    add a check

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size <= 0 || !action)
+	if (!array || size <= 0 || !action)
 		return;
 	for (i = 0; i < size; i++)
 	{
