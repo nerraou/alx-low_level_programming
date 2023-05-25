0ae8fd147f7e779b80282d658493467da67e445a
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-25 14:28:33 -0700

    print_all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,82 @@
+#include "variadic_functions.h"
+
+/**
+ * print_c - print char
+ * @list: arg
+ */
+void print_c(va_list list)
+{
+	printf("%c", (char)va_arg(list, int));
+}
+
+/**
+ * print_i - print char
+ * @list: arg
+ */
+void print_i(va_list list)
+{
+	printf("%i", va_arg(list, int));
+}
+
+/**
+ * print_f - print float
+ * @list: arg
+ */
+void print_f(va_list list)
+{
+	printf("%f", va_arg(list, double));
+}
+
+/**
+ * print_s - print string
+ * @list: arg
+ */
+void print_s(va_list list)
+{
+	char *str;
+
+	str = va_arg(list, char*);
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * print_all - print all
+ * @format: string format
+ */
+void print_all(const char *const format, ...)
+{
+	action_t actions[] = {
+		{'c', print_c},
+		{'i', print_i},
+		{'f', print_f},
+		{'s', print_s},
+		{'\0', NULL}};
+	int i;
+	int j;
+	va_list args;
+
+	va_start(args, format);
+	i = 0;
+	while (format[i])
+	{
+		j = 0;
+		while (actions[j].specifier && actions[j].specifier != format[i])
+			j++;
+		switch (actions[j].specifier)
+		{
+		case '\0':
+			break;
+
+		default:
+			if (i != 0)
+				printf(", ");
+			actions[j].f(args);
+			break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,8 +4,22 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct action - Struct action
+ *
+ * @specifier: conversion specifier
+ * @f: The function associated
+ */
+typedef struct action
+{
+	char specifier;
+	void (*f)(va_list args);
+} action_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char *const format, ...);
 
 #endif
