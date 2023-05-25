5ca2425144810fb8722f7e73c648cef57084d773
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-05-25 03:20:55 -0700

    print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print numbers
+ * @separator: to be printed between numbers
+ *@n: number of ars
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator)
+		{
+			printf("%d", va_arg(ap, int));
+			if (i + 1 < n)
+				printf("%s", separator);
+		}
+		else
+			printf("%d", va_arg(ap, int));
+	}
+	printf("\n");
+	va_end(ap);
+}
