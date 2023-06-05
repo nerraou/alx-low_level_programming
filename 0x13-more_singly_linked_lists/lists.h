d64e9806973a4201057e3f150ae653090961f66f
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-06-05 10:15:56 -0700

    fix struct name

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -13,12 +13,11 @@
  *
  * Description: singly linked list node structure
  */
-typedef struct list_s
+typedef struct listint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
