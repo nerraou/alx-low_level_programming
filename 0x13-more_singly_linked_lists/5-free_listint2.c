b46739a1197707221ebe7753219aba23abb69497
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-06-05 05:25:22 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list
+ *@head: linked list head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *to_free;
+	listint_t *it;
+
+	if (head == NULL)
+		return;
+	*it = *head;
+	while (it != NULL)
+	{
+		to_free = it;
+		it = it->next;
+		free(to_free);
+	}
+	*head = NULL;
+}
