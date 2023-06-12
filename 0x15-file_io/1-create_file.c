82fba4db2a7e8e3c2e7bf6f77e2823ce9f682df8
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-06-12 06:57:31 -0700

    for null tetx content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,11 +31,16 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t size;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
 	size = write(fd, text_content, _strlen(text_content));
 	close(fd);
 	if (size <= 0)
