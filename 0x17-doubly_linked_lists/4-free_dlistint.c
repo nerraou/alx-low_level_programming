f139b7cbe1414c27d4ed5da1f6767947289dd7e7
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-07-20 15:42:54 -0700

    free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free list
+ * @head: list head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *cur;
+	dlistint_t *next;
+
+	while (cur)
+	{
+		next = cur->next;
+		free(cur);
+		cur = next;
+	}
+}
