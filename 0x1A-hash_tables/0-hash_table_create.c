6b2f5d8766641863eb0617731f08c47b00988e3b
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-11 10:02:54 -0700

    fix allocation

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,14 +10,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *h_table;
 	unsigned long int i;
 
-	h_table = malloc(sizeof(h_table));
+	h_table = malloc(sizeof(hash_table_t));
 	if (!h_table)
 		return (NULL);
 	h_table->size = size;
-	h_table->array = NULL;
-	if (size == 0)
-		return (h_table);
-	h_table->array = malloc(sizeof(*h_table) * size);
+	h_table->array = malloc(sizeof(hash_table_t *) * size);
 	if (h_table->array == NULL)
 	{
 		free(h_table);
