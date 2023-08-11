b1fcd45245dfa1e3073d6f496b855a97cad0499f
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-11 09:28:43 -0700

    include stdlib, fix style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	h_table->size = size;
 	h_table->array = malloc(sizeof(*h_table) * size);
 	if (!(h_table->array))
+	{
+		free(h_table);
 		return (NULL);
+	}
 	for (unsigned long int i = 0; i < size; i++)
 		h_table->array[i] = NULL;
 	return (h_table);
