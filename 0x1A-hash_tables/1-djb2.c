464368ea16f4910f2752d8daf9cc50cbc55babe4
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-11 13:54:56 -0700

    add to header

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,5 @@
+#include "hash_tables.h"
+
 /**
  * hash_djb2 - implementation of the djb2 algorithm
  * @str: string used to generate hash value
@@ -15,4 +17,4 @@ unsigned long int hash_djb2(const unsigned char *str)
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
 	return (hash);
-ii}
+}
