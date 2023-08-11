ef4386aa6b65473f07387abd6b4343edee9e996c
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-11 09:36:40 -0700

    remove declaretion from the for loop

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table;
+	unsigned long int i;
 
 	h_table = malloc(sizeof(h_table));
 	if (!h_table)
@@ -19,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(h_table);
 		return (NULL);
 	}
-	for (unsigned long int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		h_table->array[i] = NULL;
 	return (h_table);
 }
