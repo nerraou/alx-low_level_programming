053ff520037a4f54828d6325dd2cdb512e22fda6
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-12 11:11:59 -0700

    cast key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -47,7 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!ht || !key || key[0] == '\0')
 		return (0);
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] != NULL)
 	{
 		head = ht->array[index];
