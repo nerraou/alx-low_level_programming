5884dd64dda6e00f71620a2b13c96dab933756ff
Nouhayla Erraou <nouhaylaerraou@gmail.com>
2023-08-12 12:53:54 -0700

    hash_table_delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,44 @@
+#include "hash_tables.h"
+
+/**
+ * free_list - free list
+ * @head: list
+ *
+ * Return: value or NULL
+ */
+void free_list(hash_node_t *head)
+{
+	hash_node_t *curr;
+
+	while (head)
+	{
+		free(head->key);
+		free(head->value);
+		curr = head;
+		head = head->next;
+		free(curr);
+	}
+	free(head);
+}
+
+/**
+ * hash_table_delete - delete hash table
+ * @ht: hash table
+ *
+ * Return: value or NULL
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index;
+
+	index = 0;
+	if (!ht)
+		return;
+	while (index < ht->size)
+	{
+		if (ht->array[index] != NULL)
+			free_list(ht->array[index]);
+		index++;
+	}
+	free(ht);
+}
